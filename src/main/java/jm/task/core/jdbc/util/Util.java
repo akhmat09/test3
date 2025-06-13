d80d336b4d331eb8358e8967804f5ce4a833f02b
Akhmat <aibaz-00@mail.ru>
2025-06-13 14:12:53 +0300

    "feat: реализованы UserDaoJDBCImpl, UserServiceImpl, Util"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test_db?useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "m2k28z";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к БД", e);
+        }
+    }
 }
